6d55ac1b3e17c9bfb598d565a12d3130c41ce143
Diem Bang <diembang09@gmail.com>
2024-06-16 23:03:57 +0200

    Add emit - player ready to join game

--- src/components/JoinGamePage.vue
+++ src/components/JoinGamePage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const username = ref<string>("");
+const username = ref("");
 
 const handleUsernameInput = (e: Event) => {
   const target = e.target as HTMLInputElement;
@@ -10,55 +10,60 @@ const handleUsernameInput = (e: Event) => {
 
 const handleJoinGame = () => {
   console.log("Player Name:", username.value);
-  
+
   // Save to local storage
-  localStorage.setItem('playerName', username.value);
+  localStorage.setItem("playerName", username.value);
+  emit('joinGame', username.value);
 };
 
+const emit = defineEmits<{ (event: 'joinGame', name: string): void }>();
 </script>
 
 <template>
-  <form @submit.prevent="handleJoinGame">
-    <div id="join-game">
-      <label for="username">Enter your name:</label>
-      <input
-        type="text"
-        id="username"
-        v-model="username"
-        @input="handleUsernameInput"
-        placeholder="Your name"
-      />
+  <div id="playerInput">
+    <h2>Enter your name to join the game</h2>
 
-      <button type="submit" @click.prevent="handleJoinGame">Join</button>
-    </div>
-  </form>
+    <form @submit.prevent="handleJoinGame">
+      <div>
+        
+        <input
+          type="text"
+          id="username"
+          v-model="username"
+          @input="handleUsernameInput"
+          placeholder="Your name"
+        />
+
+        <button type="submit" @click.prevent="handleJoinGame">Join game</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-#join-game {
+#playerInput {
   max-width: 400px;
   margin: auto;
   padding: 1em;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  
 }
 
-#join-game div {
+#playerInput div {
   margin-bottom: 1em;
 }
 
-#join-game label {
+#playerInput label {
   display: block;
   margin-bottom: 0.5em;
 }
 
-#join-game input {
+#playerInput input {
   width: 100%;
   padding: 0.5em;
   box-sizing: border-box;
 }
 
-#join-game button {
+#playerInput button {
   width: 100%;
   padding: 0.5em;
   background-color: #007bff;
@@ -68,7 +73,7 @@ const handleJoinGame = () => {
   cursor: pointer;
 }
 
-#join-game button:hover {
+#playerInput button:hover {
   background-color: #0056b3;
 }
 </style>
