a3c55e068708dbb268d7780b4b6f7292250f2301
Diem Bang <diembang09@gmail.com>
2024-06-18 14:46:26 +0200

    Add 2 players joined to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import GameBoard from './components/GameBoard.vue'
 import JoinGamePage from './components/JoinGamePage.vue'
+
+const player1 = ref<string | null>(null);
+const player2 = ref<string | null>(null);
+const playersJoined = ref(false);
+
+const handleJoinGame = (name1: string, name2: string) => {
+  player1.value = name1;
+  player2.value = name2;
+  playersJoined.value = true;
+};
 </script>
 
 <template>
   <h1>Tic Tac Toe</h1>
-  <JoinGamePage />
-  <GameBoard />
+  <div id="app">
+    <JoinGamePage v-if="!playersJoined" @joinGame="handleJoinGame" />
+    <GameBoard v-else :player1="player1" :player2="player2" />
+  </div>
+ 
 </template>
 
 <style scoped>
