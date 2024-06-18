44854bf53791556d93d4aef217464ea6480a955d
Diem Bang <diembang09@gmail.com>
2024-06-18 23:43:42 +0200

    Add check for winner function

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -22,6 +22,41 @@ const handleSquareClick = (index: number) => {
   }
 };
 
+const addScore = (index: number) => {};
+
+const checkForWinner = () => {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (
+      board.value[a] &&
+      board.value[a] === board.value[b] &&
+      board.value[a] === board.value[c]
+    ) {
+      if (board.value[a] === player1Symbol.value) {
+        status.value = `${props.player1} wins!`;
+      } else {
+        status.value = `${props.player2} wins!`;
+      }
+
+      // To-do: add players score
+      addScore(a);
+      return;
+    }
+  }
+  if (!board.value.includes(null)) {
+    status.value = "It's a draw!";
+  }
+};
 
 
 
