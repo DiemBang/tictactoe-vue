7feede7aac64533521649e8e32b4fe8751805849
Diem Bang <diembang09@gmail.com>
2024-06-18 16:05:38 +0200

    create square + emit click event

---
+++ src/components/Square.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+const props = defineProps<{ value: string | null }>();
+const emit = defineEmits<{ (event: "click"): void }>();
+
+const handleClick = () => {
+  if (!props.value) {
+    emit("click");
+  }
+};
+</script>
+
+<template>
+  <div class="square" @click="handleClick">
+    {{ value }}
+  </div>
+</template>
+
+<style scoped>
+.square {
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2em;
+  border: 1px solid #000;
+  cursor: pointer;
+}
+</style>
